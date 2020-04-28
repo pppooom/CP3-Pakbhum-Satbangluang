bf0b2843711d5e448c295ca0e2f95841d4e16ba4
pppooom <64434594+pppooom@users.noreply.github.com>
2020-04-28 16:18:25 +0700

    Add files via upload

---
+++ Exercise_Pakbhum_S.py
@@ -0,0 +1,10 @@
+EngSc = 40.51
+BusSc = 50.00
+IntroComSc = 44.53
+ComProSc = 30.11
+print("---Yor Score---")
+print("Foundation English               :",EngSc)
+print("General Business                 :",BusSc)
+print("Introduction to Computer Systems :",IntroComSc)
+print("Computer Programming             :",ComProSc)
+
