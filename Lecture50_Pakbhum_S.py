d26570c39cbdfd808105b8762e4eff1e7ee8a3cf
pppooom <64434594+pppooom@users.noreply.github.com>
2020-04-29 18:19:06 +0700

    Add files via upload

---
+++ Lecture50_Pakbhum_S.py
@@ -0,0 +1,12 @@
+def addNumber(x,y):
+    print(x+y)
+def substracNumber(x,y):
+    print(x-y)
+def multipleNumber(x,y):
+    print(x*y)
+def divideNumber(x,y):
+    print(int(x/y))
+addNumber(100,4)
+substracNumber(100,4)
+multipleNumber(100,4)
+divideNumber(100,4)
